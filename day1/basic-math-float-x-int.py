4175dce06ee0bbd5b9bc6a775ee92b2df69be109
nixhal33 <nischal.koirala33@gmail.com>
2026-06-22 11:45:29 +0545

    basic math floating and int combined

---
+++ day1/basic-math-float-x-int.py
@@ -0,0 +1,32 @@
+print("Basic Math with Floats and Integers")
+print("-----------------------------------")
+print("\nNow with floats:")
+n1=15.8
+n2=20.5
+
+# Basic Arthemetic Operations
+
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+
+print("\nNow with integers:")
+print("-------------------")
+n1 = 13
+n2 = 20
+
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
