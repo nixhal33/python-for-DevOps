db8d2e8442bfd52355dbbb9541d84afe12e2cfaa
nixhal33 <nischal.koirala33@gmail.com>
2026-06-22 11:30:43 +0545

    str search py code written

---
+++ day1/str-search.py
@@ -0,0 +1,7 @@
+sentence = "Vince Kenedy McMahon"
+word = "Kenedy"
+if word in sentence:
+    print(f"The word '{word}' is present in the sentence.")
+
+# Output: The word 'Kenedy' is present in the sentence. which means the word is found in the sentence.
+# simply this function is to check if a specific word is present in a given sentence or not. It uses the 'in' keyword to check for the presence of the word in the sentence. If the word is found, it prints a message indicating that the word is present in the sentence.
