7f3ce959460efee9c8b9c699c6493f60cda127e2
nixhal33 <nischal.koirala33@gmail.com>
2026-06-22 11:02:07 +0545

    string lowerupper with concat

---
+++ day1/string_lowercase.py
@@ -0,0 +1,11 @@
+a = "I am superman"
+b = "I am batman"
+c = "I am spiderman"
+
+uc = a.upper()
+lc = b.lower()+" "+c.upper()
+print(lc)
+print(uc)
+
+# Output: i am batman I AM SPIDERMAN 
+# I AM SUPERMAN
