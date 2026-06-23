5b47e23bf2693b885dee9d4dde78cf2685b9b05e
nixhal33 <nischal.koirala33@gmail.com>
2026-06-23 11:25:47 +0545

    import module learned

--- day2/advance-calculator.py
+++ day2/advance-calculator.py
@@ -1,8 +1,7 @@
 import calculator as adv_calc
 
-adv_calc.add(2, 6)
-adv_calc.subtract(1, 10)
-adv_calc.multiply(10, 5)
-adv_calc.divide(30, 20)
-adv_calc.modulo(4, 40)
-
+print(f"Addition: {adv_calc.add(2, 6)}")
+print(f"Subtraction: {adv_calc.subtract(1, 10)}")
+print(f"Multiplication: {adv_calc.multiply(10, 5)}")
+print(f"Division: {adv_calc.divide(30, 20)}")
+print(f"Modulo: {adv_calc.modulo(4, 40)}")
