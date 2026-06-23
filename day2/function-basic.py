3623ecd6bfc0f191c32113db5e2340bf8686c0e1
nixhal33 <nischal.koirala33@gmail.com>
2026-06-23 10:41:22 +0545

    basic function practice

---
+++ day2/function-basic.py
@@ -0,0 +1,27 @@
+a = 5
+b = 10
+
+def add():
+    added_value = a + b
+    print(added_value)
+
+def subtract():
+    subtracted_value = b - a
+    print(subtracted_value)
+
+def multiply():
+    multiplied_value = a * b
+    print(multiplied_value)
+
+def divide():
+    divided_value = b / a
+    print(divided_value)
+
+def modulus():
+    modulus_value = b % a
+    print(modulus_value)
+
+add()
+subtract()
+multiply()
+divide()
