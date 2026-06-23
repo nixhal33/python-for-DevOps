956920113b0a2444cd95e427106498e2d5107271
nixhal33 <nischal.koirala33@gmail.com>
2026-06-23 17:26:17 +0545

    learning list and tuples

---
+++ day-4-tuples&list/list.py
@@ -0,0 +1,5 @@
+# lists and they are mutable ie: once created it's value can be changed(add or remove both can be happened)
+
+techhaus_employees = ["kushal","sagar","bibek","nirajan","bishop","namuna"]
+print("Techhaus Employees:-", techhaus_employees)
+
