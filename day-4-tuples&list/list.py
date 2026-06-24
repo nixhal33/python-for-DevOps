89b621c19857cece9bb3d7befed02b67c5f501aa
nixhal33 <nischal.koirala33@gmail.com>
2026-06-24 09:20:13 +0545

    list & tuples practiced

--- day-4-tuples&list/list.py
+++
@@ -1,5 +0,0 @@
-# lists and they are mutable ie: once created it's value can be changed(add or remove both can be happened)
-
-techhaus_employees = ["kushal","sagar","bibek","nirajan","bishop","namuna"]
-print("Techhaus Employees:-", techhaus_employees)
-
